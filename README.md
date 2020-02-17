e420ba946fe748546a7ae46b4619f7e682bd2a37
yangkq <yangkqandy@163.com>
2020-02-17 14:53:10 +0800

    delete

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# gateway-websocket-webservic
+## 启动顺序：
+
+1.eureka 1025
+
+2.config 1026
+
+3.gateway 8888
+
+4.bullet-websocket 5678
+
+5.hello-webservice 5679
+
+验证websocket
+
+http://127.0.0.1:5678/index.html
+查看项目 buulet-websocket 中/resources/static/index.html
+请求地址通过gateway http://127.0.0.1:8888";
+
+
+验证webservice
+
+http://127.0.0.1:8888/hello/Service/hello?wsdl
+
+
+
---
+++ bullet-websocket/src/main/java/com/anchnet/bullet/BulletMessageDTO.java
@@ -0,0 +1,24 @@
+package com.anchnet.bullet;
+
+
+
+public class BulletMessageDTO {
+    private String username;
+    private String message;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
